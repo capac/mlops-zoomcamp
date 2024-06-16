da5b5fdaf9ffd077d5cb461bf80b4d3be9f59269
capac <capac@users.noreply.github.com>
2024-06-16 10:26:18 +0100

    'starter.py' script for question 5

--- 2024/homework/04-deployment/starter.py
+++ 2024/homework/04-deployment/starter.py
@@ -5,71 +5,40 @@ import pickle
 import pandas as pd
 import numpy as np
 from pathlib import Path
+from sys import argv
 
 with open('model.bin', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
 categorical = ['PULocationID', 'DOLocationID']
 
-def read_data(filename):
-    df = pd.read_parquet(filename)
-    
+
+def read_data(data_link):
+    print('Downloading data...')
+    df = pd.read_parquet(data_link)
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
-
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
-
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
+    print('Returning dataframe...')
     return df
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# ## Question 1
-# 
-# What's the standard deviation of the predicted duration for this dataset?
-# 
-# * 1.24
-# * **6.24**
-# * 12.28
-# * 18.28
-
-print(f'The standard deviation is {round(np.std(y_pred), 2)}.')
+def calculate_prediction(df):
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
+    print('Calculating predictions...')
+    return y_pred
 
 
-# ## Question 2
-# 
-# What's the size of the output file?
-# 
-# * 36M
-# * 46M
-# * 56M
-# * **66M**
+def main():
+    year, month = argv[1], argv[2]
+    data_link = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet'
+    df = read_data(data_link)
+    y_pred = calculate_prediction(df)
+    print(f'The mean prediction is {round(np.mean(y_pred), 2)}.')
 
 
-year, month = 2023, 3
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-df_results = pd.concat([df['ride_id'], pd.DataFrame(y_pred)])
-
-
-df_results.to_parquet(
-    'output.parquet',
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-print(f'''The size of the output file is {round(Path('output.parquet').stat().st_size/(1024*1024), 2)} MB.''')
-
-
-# ## Q3. Creating the scoring script
-# 
-# Now let's turn the notebook into a script. Which command you need to execute for that?
+if __name__ == "__main__":
+    main()
