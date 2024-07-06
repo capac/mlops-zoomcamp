7e74cbd59e6a0702c549f4230cd4b2abb2b3caa3
capac <541813+capac@users.noreply.github.com>
2024-07-06 11:18:48 +0000

    Added 'test_prepare_features'

--- 2024/homework/06-best-practices/code/tests/model_test.py
+++ 2024/homework/06-best-practices/code/tests/model_test.py
@@ -13,3 +13,20 @@ def test_base64_decode():
         "ride_id": 256
     }
     assert actual_result == expected_result
+
+
+def test_prepare_features():
+    model_service = model.ModelService(None)
+    ride = {
+        "PULocationID": 130,
+        "DOLocationID": 205,
+        "trip_distance": 3.66,
+    }
+    actual_features = model_service.prepare_features(ride)
+
+    expected_features = {
+        "PU_DO": "130_205",
+        "trip_distance": 3.66,
+    }
+
+    assert actual_features == expected_features
