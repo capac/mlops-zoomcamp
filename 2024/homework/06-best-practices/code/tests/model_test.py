80b268369b7900f5540c58af50903e207cafbf04
capac <541813+capac@users.noreply.github.com>
2024-07-06 11:18:12 +0000

    Added 'test_base64_decode'

---
+++ 2024/homework/06-best-practices/code/tests/model_test.py
@@ -0,0 +1,15 @@
+import model
+
+
+def test_base64_decode():
+    base64_input = "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDI1NgogICAgfQ=="
+    actual_result = model.base64_decode(base64_input)
+    expected_result = {
+        "ride": {
+            "PULocationID": 130,
+            "DOLocationID": 205,
+            "trip_distance": 3.66
+        }, 
+        "ride_id": 256
+    }
+    assert actual_result == expected_result
