8f56938a61b4369ffb5580afc16b6f63194bbdfa
capac <541813+capac@users.noreply.github.com>
2024-07-06 11:19:13 +0000

    Added 'test_predict'

--- 2024/homework/06-best-practices/code/tests/model_test.py
+++ 2024/homework/06-best-practices/code/tests/model_test.py
@@ -30,3 +30,28 @@ def test_prepare_features():
     }
 
     assert actual_features == expected_features
+
+
+class ModelMock:
+    def __init__(self, value):
+        self.value = value
+
+    def predict(self, X):
+        n = len(X)
+        return [self.value] * n
+
+
+def test_predict():
+    model = ModelMock(10.0)
+
+    model_service = model.ModelService(None)
+    
+    features = {
+        "PU_DO": "130_205",
+        "trip_distance": 3.66,
+    }
+
+    actual_prediction = model_service.predict(features)
+    expected_prediction = 10.0
+
+    assert actual_prediction == expected_prediction
