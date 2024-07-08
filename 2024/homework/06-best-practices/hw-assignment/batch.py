be168abb0c076f541e3465211109396b1d4db065
capac <capac@users.noreply.github.com>
2024-07-08 16:24:27 +0100

    Refactored code

--- 2024/homework/06-best-practices/hw-assignment/batch.py
+++ 2024/homework/06-best-practices/hw-assignment/batch.py
@@ -6,26 +6,10 @@ import pickle
 import pandas as pd
 
 
-year = int(sys.argv[1])
-month = int(sys.argv[2])
-
-basr_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
-
-input_file = f'{basr_url}/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-
-
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-categorical = ['PULocationID', 'DOLocationID']
-
-
-def read_data(filename):
+def read_data(filename, categorical):
     df = pd.read_parquet(filename)
 
-    df[
-        'duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
@@ -35,18 +19,36 @@ def read_data(filename):
     return df
 
 
-df = read_data(input_file)
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+def main(year, month):
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+    print('predicted mean duration:', y_pred.mean())
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+
+    df_result.to_parquet(output_file, engine='pyarrow', index=False)
 
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
+if __name__ == "__main__":
 
-print('predicted mean duration:', y_pred.mean())
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
 
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
+    categorical = ['PULocationID', 'DOLocationID']
 
-df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+
+    base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
+
+    input_file = f'{base_url}/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+    main(year, month)
