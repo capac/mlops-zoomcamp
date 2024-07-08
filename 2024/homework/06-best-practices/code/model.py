9580ea2be722a6ff4c9b3ca7de12e01618471e1a
capac <capac@users.noreply.github.com>
2024-07-08 18:13:35 +0100

    Added options to init model service

--- 2024/homework/06-best-practices/code/model.py
+++ 2024/homework/06-best-practices/code/model.py
@@ -96,5 +96,9 @@ def init(prediction_stream_name: str, run_id: str, test_run: bool):
         )
         callbacks.append(kinesis_callback.put_record)
 
-    model_service = ModelService(model)
+    model_service = ModelService(
+        model=model,
+        model_version=run_id,
+        callbacks=callbacks,
+    )
     return model_service
